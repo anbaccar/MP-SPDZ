f00fe0eda5a037ca18375fd10de66001f378291c
Alessandro Baccarini <baccaan@gmail.com>
2022-06-05 18:28:15 -0400

    updated mcomp

--- 3pc_benchmark_mcomp.sh
+++ 3pc_benchmark_mcomp.sh
@@ -4,13 +4,13 @@ timestamp=$(date -d "today" +"%Y%m%d%H%M")
 echo $timestamp
 
 
-declare -a batch_sizes=("1" "10" "100" "1000" "10000" "100000" "1000000")
-# declare -a batch_sizes=("1" "10")
+# declare -a batch_sizes=("1" "10" "100" "1000" "10000" "100000" "1000000")
+declare -a batch_sizes=("1" "10")
 declare -a ring_sizes=("32" "64")
 
 for batch in "${batch_sizes[@]}"; do
     for ring in "${ring_sizes[@]}"; do
-        ./compile.py -R 64 eda-bench $ring $batch
+        ./compile.py -R 64 mcomp 2 1 $batch $ring_sizes
     done
 done
 
